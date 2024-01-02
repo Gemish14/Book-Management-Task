1ab0b35bce17721a4cd4c3978e2265dde296fb4a
Jeet Joshi <jejo14501@gmail.com>
2024-01-03 01:47:24 +0530

    Updated urls.py

--- BookManagementProject/home/urls.py
+++ BookManagementProject/home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
+from home import views
 
 urlpatterns = [
-    
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('login',views.login,name='login'),
+    path('bookView',views.bookView,name='bookView'),
+    path('AllBooks/',views.AllBooks,name = "AllBooks")
 ]
