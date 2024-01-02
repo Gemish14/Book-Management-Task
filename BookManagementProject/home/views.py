03d0f98200817e5990d29b20042e2a7fe433b704
Jeet Joshi <jejo14501@gmail.com>
2024-01-03 01:47:42 +0530

    Modified views.py

--- BookManagementProject/home/views.py
+++ BookManagementProject/home/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from home.models import Book
 
-# Create your views here.
 def home(request):
-    return render(request, "home.html", )
+    return render (request,'home.html')
+
+def login(request):
+    return render (request,'login.html')
+
+def bookView(request):
+    return render (request,'bookView.html')
+
+def AllBooks(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'AllBooks.html', {'books': books})
+    #return render (request,'AllBooks.html')
+    
