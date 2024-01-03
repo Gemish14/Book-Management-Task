b8a7dd48c3deb4af3d52543caf4cc125dbc2b185
Jeet Joshi <jejo14501@gmail.com>
2024-01-03 20:14:52 +0530

    New Changes 3/1/24 8:10 PM

--- BookManagementProject/home/admin.py
+++ BookManagementProject/home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from home.models import Book
+from home.models import Book,Student
 
 admin.site.register(Book) #register our model
+admin.site.register(Student) #register our model
--- BookManagementProject/home/models.py
+++ BookManagementProject/home/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import secrets
 
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
@@ -7,14 +6,21 @@ class Book(models.Model):
     author = models.CharField(max_length=50)
     pagesCount = models.IntegerField()
     price = models.IntegerField()
-    image = models.ImageField(upload_to='static/uploadedImages/',default='static/uploadedImages/default.jpeg')
+    category = models.CharField(max_length=10, default="Unknown Category")
+    image = models.ImageField(upload_to='uploaded_images/',default='static/uploadedImages/default.jpg')
 
     def __str__(self):
         return self.title 
     
 class Student(models.Model):
-    name = models.CharField(max_length=50)
-    stud_id = models.IntegerField(primary_key=True)
+    name = models.CharField("Enter Full Name", max_length=50)
+    stud_id = models.IntegerField("Enter Roll No.",primary_key=True)
+    email = models.EmailField("Enter Email",null=True)
 
-    def __str__(self):
+    """ def __str__(self):
         return self.name 
+ """
+    class Meta : 
+        db_table = 'student'
+
+    
