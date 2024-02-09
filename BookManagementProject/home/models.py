708271e2f4c9b5a22aee528b9a82b1fdb88186f7
Jeet Joshi <jejo14501@gmail.com>
2024-02-09 13:29:03 +0530

    .

--- BookManagementProject/home/models.py
+++ BookManagementProject/home/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+""" from django.contrib.auth.models import User
+"""
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     pagesCount = models.IntegerField()
     price = models.IntegerField()
-    category = models.CharField(max_length=30, default="Unknown Category")
+    category = models.CharField(max_length=10, default="Unknown Category")
     image = models.ImageField(upload_to='uploaded_images/',default='static/uploadedImages/default.jpg')
 
     def __str__(self):
         return self.title 
     
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """ user = models.OneToOneField(User, on_delete=models.CASCADE) """
     name = models.CharField("Enter Full Name", max_length=50)
     stud_id = models.IntegerField("Enter Roll No.",primary_key=True)
     email = models.EmailField("Enter Email",null=True)
@@ -26,12 +26,12 @@ class Student(models.Model):
     class Meta : 
         db_table = 'student' 
 
-class Transaction(models.Model):
+""" class Transaction(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     transaction_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.student.username} reserved {self.book.title}"
-
+ """
     
